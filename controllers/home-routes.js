405b7c7319955c0431f1beb4ea47b22e65016ea4
Christian Payan <payanstocks@gmail.com>
2021-05-07 20:14:21 -0700

    Added route to /stats

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,4 +5,8 @@ router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/html/index.html"));
 });
 
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/stats.html"));
+});
+
 module.exports = router;
