5cf800ac50c9467ed0ea7a5fba50475fe5078289
Christian Payan <payanstocks@gmail.com>
2021-05-07 20:17:56 -0700

    made sure model matched seeds

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-
 const { Schema } = mongoose;
-
 const WorkoutSchema = new Schema(
     {
         day: {
@@ -10,13 +8,32 @@ const WorkoutSchema = new Schema(
         },
         exercises: [
             {
-                type: { type: String, trim: true, required: true },
-                name: { type: String, trim: true, required: true },
-                duration: { type: Number, required: true },
-                weight: { type: Number },
-                repetitions: { type: Number },
-                sets: { type: Number },
-                distance: { type: Number },
+                type: {
+                    type: String,
+                    trim: true,
+                    required: true
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: true
+                },
+                duration: {
+                    type: Number,
+                    required: true
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                },
             },
         ],
     },
@@ -26,13 +43,10 @@ const WorkoutSchema = new Schema(
         },
     }
 );
-
 WorkoutSchema.virtual("totalDuration").get(function () {
     return this.exercises.reduce((total, exercise) => {
         return total + exercise.duration;
     }, 0);
 });
-
 const Workout = mongoose.model("Workout", WorkoutSchema);
-
 module.exports = Workout;
