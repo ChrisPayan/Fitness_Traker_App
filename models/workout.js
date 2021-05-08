c0527f3787ea1ced60248f8e7c90e4ce12a201db
Christian Payan <payanstocks@gmail.com>
2021-05-07 19:41:43 -0700

    Fleshed out models for workout js

---
+++ models/workout.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now,
+        },
+        exercises: [
+            {
+                type: { type: String, trim: true, required: true },
+                name: { type: String, trim: true, required: true },
+                duration: { type: Number, required: true },
+                weight: { type: Number },
+                repetitions: { type: Number },
+                sets: { type: Number },
+                distance: { type: Number },
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
