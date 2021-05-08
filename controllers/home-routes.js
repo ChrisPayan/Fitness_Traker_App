5f860dbc78628ca28830e39d1de8446505f09e77
Christian Payan <payanstocks@gmail.com>
2021-05-07 21:33:02 -0700

    Fixed a bug

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,4 +9,8 @@ router.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/html/stats.html"));
 });
 
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/exercise.html"));
+});
+
 module.exports = router;
